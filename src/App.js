4c2034a9300500ad77b2d8e6071fd66f9c05da21
Jennxfern-n <jennifernavarro181@gmail.com>
2025-10-28 14:02:02 -0600

    Arreglo de rutas y comienzo de navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Navbar/Navbar';
 import Home from './Pages/Home';
 import Education from './Pages/Education';
 import Projects from './Pages/Projects';
 import Skills from './Pages/Skills';
+
 function App() {
+  const showNavbar = true; 
+
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        {showNavbar && <NavBar />}
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Education" element={<Education />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Skills" element={<Skills />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
